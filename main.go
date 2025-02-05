abb9069b8af203aa509de1a581df3022383c1fd4
Karol Moroz <karol@moroz.dev>
2025-02-05 18:08:47 +0800

    Create a bucket

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil
+		_, err := s3.NewBucketV2(ctx, "example", &s3.BucketV2Args{
+			Bucket: pulumi.String("moroz-pulumi-demo-static-bucket"),
+		})
+
+		return err
 	})
 }
